74df2d7e6880884fd1e7fadf09dc9889092d99d8
MOUSSA MBAYE <moussa.mbaye74@yahoo.com>
2023-03-11 03:49:02 -0500

    Initial Commit

---
+++ SarturdayMar11at345/SarturdayMar11at345App.swift
@@ -0,0 +1,17 @@
+//
+//  SarturdayMar11at345App.swift
+//  SarturdayMar11at345
+//
+//  Created by moussa mbaye on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SarturdayMar11at345App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
